6f772e625130033ad4c70543ea273ccd998cdca7
Frank Uhlig <uhlig.frank@gmail.com>
2022-07-05 22:28:02 +0200

    add: module.mk for including from top of Alienhack

---
+++ module.mk
@@ -0,0 +1,19 @@
+SRC += RL-Shared/ActionEngine/ActionEngine.cpp \
+       RL-Shared/Algorithm/Algorithms-2D.cpp \
+       RL-Shared/Console/Console.cpp \
+       RL-Shared/ConsoleView/ConsoleView.cpp \
+       RL-Shared/ConsoleView/KeyMap.cpp \
+       RL-Shared/Game/GameModel.cpp \
+       RL-Shared/Game/GameObject.cpp \
+       RL-Shared/Interface/InterfaceState.cpp \
+       RL-Shared/Interface/InterfaceStateMachine.cpp \
+       RL-Shared/Messages/Console/MessageDisplay.cpp \
+       RL-Shared/Messages/UserMessageLog.cpp \
+       RL-Shared/World-2DTiles/BSPMapGeneration/BSPMapMake.cpp \
+       RL-Shared/World-2DTiles/BSPMapGeneration/BSPMapMakeWrapper.cpp \
+       RL-Shared/World-2DTiles/HearingField.cpp \
+       RL-Shared/World-2DTiles/VisionField.cpp \
+       RL-Shared/World-2DTiles/World.cpp \
+       RL-Shared/World-2DTiles/Zone.cpp \
+       RL-Shared/WorldObjects/Actor.cpp \
+       RL-Shared/WorldObjects/WorldObject.cpp
