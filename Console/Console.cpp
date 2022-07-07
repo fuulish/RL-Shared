997cd0131890292704336394ef0be57c1585bca2
Frank Uhlig <uhlig.frank@gmail.com>
2022-07-07 20:43:03 +0200

    chg: substitute windows headers by ncurses

--- Console/Console.cpp
+++ Console/Console.cpp
@@ -1,10 +1,11 @@
 #include "Console.hpp"
 
 #define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+// #include <windows.h>
+// #include <conio.h>
+#include <curses.h>
 
 #include <stdio.h>
-#include <conio.h>
 
 
 
