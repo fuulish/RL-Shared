828d4751d9853cec70c09b9b50c194e3d2237464
Frank Uhlig <uhlig.frank@gmail.com>
2023-06-19 20:29:58 +0200

    chg(config): acknowledge apple as Unix

--- Include/system.hpp
+++ Include/system.hpp
@@ -5,7 +5,7 @@
 #ifdef _DEBUG
 #else //_DEBUG
 
-#if defined(unix) || defined(__unix__) || defined(__unix)
+#if defined(unix) || defined(__unix__) || defined(__unix) || defined(__APPLE__)
 #define IS_UNIX
 #elif defined(_WIN32)
 #define IS_WINDOWS
